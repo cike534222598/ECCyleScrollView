f9c317b6f63297a02cce94a2b7db59aa2f8c589c
Shaco <cike534222598@qq.com>
2021-01-13 14:56:25 +0800

    新增示例代码

--- Example/ECCyleScrollView/ECViewController.m
+++ Example/ECCyleScrollView/ECViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ECViewController.h"
+#import "ECCycleScrollView.h"
 
-@interface ECViewController ()
+@interface ECViewController () <ECCycleScrollViewDelegate, ECCycleScrollViewDataSource>
+
+@property (nonatomic, strong) ECCycleScrollView *cycleView;
 
 @end
 
@@ -18,6 +21,13 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.cycleView = [[ECCycleScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 200) withTimer:YES withInfiniteLoop:YES withPageControl:NO];
+    self.cycleView.delegate = self;
+    self.cycleView.dataSource = self;
+    [self.view addSubview:self.cycleView];
+    
+    [self.cycleView reloadData];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +35,20 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger)numberOfPages {
+    return 5;
+}
+
+- (UIView *)pageAtIndex:(NSInteger)index {
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 200)];
+    label.backgroundColor = [UIColor orangeColor];
+    label.text = [NSString stringWithFormat:@"这是第%ld张banner", index];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.textColor = [UIColor blackColor];
+    label.font = [UIFont systemFontOfSize:15];
+    return label;
+}
+
 
 @end
